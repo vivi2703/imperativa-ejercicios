7145c3b5ba4e004013e799e9e34c80e059727f3c
peter3110 <pedro3110.jim@gmail.com>
2023-03-02 18:15:21 -0300

    edad

--- prompt.js
+++ prompt.js
@@ -1,8 +1,15 @@
 var prompt = require('prompt-sync')();
 
+/*
 var nombre = prompt('Ponga su nombre: ');
 console.log(nombre);
 
 var edad = prompt('Ponga su edad: ');
-texto = 'Hola ' + nombre + ', su edad es ' + edad + 'anios';
+texto = 'Hola ' + nombre + ', su edad es ' + edad + ' anios';
 console.log(texto);
+*/
+
+///////////////////////////////////
+// Ejercicio en clase
+var nombre = prompt('Ponga su nombre: ');
+console.log('Hola ' + nombre);
